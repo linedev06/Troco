07c1218f02e3b6669bd224b5a4962ce18756f2fd
linedev06 <vitorafelismino@gmail.com>
2023-04-01 00:50:24 -0300

    exercicio feito

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--Troco--");
+decimal valorcompra, valorpago, valortroco;
+
+Console.Write("Valor compra R$:");
+valorcompra = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Valor pago R$:");
+valorpago = Convert.ToDecimal(Console.ReadLine());
+
+valortroco = valorpago - valorcompra;
+
+Console.Write($"\nTroco: {valortroco}");
